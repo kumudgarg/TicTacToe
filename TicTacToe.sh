830990ba1330ba3a241019188dfcbf8cbbd1b592
kumudgarg <kumud50garg@gmail.com>
2019-11-18 18:26:19 +0530

    Refactor : only player move did check valid and unvalid

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -2,11 +2,13 @@
 ROW=3
 COLUMN=3
 playPosition=0
-computer=''
-player=''
+computerSymbol=""
+playerSymbol=""
 randNum=0
+arrLen=$(( $ROW * $COLUMN ))
 toss=$(( RANDOM%2 ))
 randNum=$(( RANDOM % 2 ))
+count=0
 declare -A board
 function resetBoard()
 {
@@ -14,9 +16,8 @@ function resetBoard()
 	do
 		for (( j=0; j<$COLUMN; j++ ))
 		do
-			playPosition=$(( playPosition + 1 ))
-			board[$i,$j]=$playPosition
-				
+			count=$(( $count + 1 ))
+			board[$i,$j]=$count
 		done
 	done
 }
@@ -24,38 +25,70 @@ function symbolAssign()
 {
 	if [ $randNum -eq 0 ]
 	then
-		computer='0'
-		player='x'
-		echo $computer $player
+		computerSymbol="0"
+		playerSymbol="x"
+		echo $computerSymbol $playerSymbol
 	else 
-		computer='x'
-		player='0'
-		 echo $computer $player
+		computerSymbol="x"
+		playerSymbol="0"
+		 echo $computerSymbol $playerSymbol
 	fi
 }
 function whoPlayFirst()
 {
 	if [ $toss -eq 0 ]
 	then
-			echo "computer will play first"
+			echo "computerSymbol will play first"
+			computerSymbol="0"
+      	playerSymbol="x"
 	else
-			echo "player will play first"
+			echo "playerSymbol will play first"
+			computerSymbol="x"
+      	playerSymbol="0"
+
 	fi
 }
 function displayBoard()
 {
+	count=0
 	for (( i=0; i<$ROW; i++ ))
    do
       for (( j=0; j<$COLUMN; j++ ))
-      do
-			echo -n "|  $(( board[$i,$j] ))   | "
+      do 
+				echo -n "|   ${board[$i,$j]}    | "
 		done
 			printf "\n"
 			echo  -n " ------   ------   ------ "
 			printf "\n"
 	done
 }
+function playTicTacToe()
+{
+	whoPlayFirst
+
+	for (( k=0; k<arrLen; k++ ))
+	do
+		displayBoard
+      read -p " enter player choice " playerChoice
+		for (( i=0; i<$ROW; i++ ))
+		do
+			for (( j=0; j<$COLUMN; j++ ))
+			do
+				while [  $((${board[$i,$j]})) -eq $(($playerSymbol)) ]
+            do
+						echo "Invalid move"
+						displayBoard
+      				read -p " enter playerSymbol choice " playerChoice
+				done
+				if [ $((${board[$i,$j]})) -eq $playerChoice ] 
+				then
+				 	board[$i,$j]=$playerSymbol 
+				fi
+			done
+      done
+	done
+}
 resetBoard
 symbolAssign
-whoPlayFirst
+playTicTacToe
 displayBoard
