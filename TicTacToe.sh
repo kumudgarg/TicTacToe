b1021a8c994f48c1d41bfb1d245c085f37366907
kumudgarg <kumud50garg@gmail.com>
2019-11-19 15:11:01 +0530

    Determine win condition, tie or change the turn

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -1,12 +1,18 @@
 #!/bin/bash -x
+#<------Constants------------------->
 ROW=3
 COLUMN=3
+ARRAYLEN=$(( $ROW * $COLUMN ))
+#<---------Variables----------------->
 playPosition=0
-computer=''
-player=''
+computerSymbol=""
+playerSymbol=""
 randNum=0
 toss=$(( RANDOM%2 ))
 randNum=$(( RANDOM % 2 ))
+row=0
+column=0
+valid=false
 declare -A board
 function resetBoard()
 {
@@ -14,9 +20,8 @@ function resetBoard()
 	do
 		for (( j=0; j<$COLUMN; j++ ))
 		do
-			playPosition=$(( playPosition + 1 ))
-			board[$i,$j]=$playPosition
-				
+			count=$(( $count + 1 ))
+			board[$i,$j]=$count
 		done
 	done
 }
@@ -24,38 +29,106 @@ function symbolAssign()
 {
 	if [ $randNum -eq 0 ]
 	then
-		computer='0'
-		player='x'
-		echo $computer $player
+		computerSymbol="0"
+		playerSymbol="x"
+		echo $computerSymbol $playerSymbol
 	else 
-		computer='x'
-		player='0'
-		 echo $computer $player
+		computerSymbol="x"
+		playerSymbol="0"
+		 echo $computerSymbol $playerSymbol
 	fi
 }
 function whoPlayFirst()
 {
 	if [ $toss -eq 0 ]
 	then
-			echo "computer will play first"
+			echo "computerSymbol will play first"
+			computerSymbol="0"
+      	playerSymbol="x"
 	else
-			echo "player will play first"
+			echo "playerSymbol will play first"
+			computerSymbol="x"
+      	playerSymbol="0"
+
 	fi
 }
 function displayBoard()
 {
+	count=0
 	for (( i=0; i<$ROW; i++ ))
    do
       for (( j=0; j<$COLUMN; j++ ))
-      do
-			echo -n "|  $(( board[$i,$j] ))   | "
+      do 
+				echo -n "|   ${board[$i,$j]}    | "
 		done
 			printf "\n"
 			echo  -n " ------   ------   ------ "
 			printf "\n"
 	done
 }
+function playTicTacToe()
+{
+ whoPlayFirst
+ for (( k=0; k<ARRAYLEN; k++ ))
+ do
+	displayBoard
+	read -p " enter player choice " playerChoice
+	row=$(( $playerChoice / $ROW ))
+	if [ $(( $playerChoice % $ROW )) -eq 0 ]
+	then
+		row=$(( $row - 1 ))
+	fi 
+	column=$(( $playerChoice %  $COLUMN ))
+	if [ $column -eq 0 ]
+	then
+	column=$(( $column + 2 ))
+	else
+		column=$(( $column - 1 ))
+	fi
+	if [ $((${board[$row,$column]})) -eq $(($playerSymbol)) ]
+	then
+		echo "Invalid move"
+		(( k-- ))
+	fi
+	board[$row,$column]=$playerSymbol
+	isCheckResult
+	if [ $valid == true ]
+        then
+		displayBoard
+                echo "you are won"
+                return 0
+         fi 
+ done
+	echo "game Tie"
+}
+function isCheckResult()
+{
+	if [ ${board[0,0]} == $playerSymbol ] && [ ${board[0,1]} == $playerSymbol ] && [ ${board[0,2]} == $playerSymbol ]
+	then
+		valid=true
+	elif [ ${board[1,0]} == $playerSymbol ] && [ ${board[1,1]} == $playerSymbol ] && [ ${board[1,2]} == $playerSymbol ]
+        then
+                valid=true
+	elif [ ${board[2,0]} == $playerSymbol ] && [ ${board[2,1]} == $playerSymbol ] && [ ${board[2,2]} == $playerSymbol ]
+        then
+		  valid=true
+	elif [ ${board[0,0]} == $playerSymbol ] && [ ${board[1,0]} == $playerSymbol ] && [ ${board[2,0]} == $playerSymbol ]
+        then
+                 valid=true
+	elif [ ${board[0,1]} == $playerSymbol ] && [ ${board[1,1]} == $playerSymbol ] && [ ${board[2,1]} == $playerSymbol ]
+        then
+                 valid=true
+	elif [ ${board[0,2]} == $playerSymbol ] && [ ${board[1,2]} == $playerSymbol ] && [ ${board[2,2]} == $playerSymbol ]
+        then
+		 valid=true
+        elif [ ${board[0,0]} == $playerSymbol ] && [ ${board[1,1]} == $playerSymbol ] && [ ${board[2,2]} == $playerSymbol ]
+        then
+              valid=true
+	elif [ ${board[0,2]} == $playerSymbol ] && [ ${board[1,1]} == $playerSymbol ] && [ ${board[2,0]} == $playerSymbol ]
+        then
+                valid=true
+	fi
+}
 resetBoard
 symbolAssign
-whoPlayFirst
-displayBoard
+playTicTacToe
