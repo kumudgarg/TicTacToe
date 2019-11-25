e12c849d6d2290a3168acc1e475bbf585bbe9a4d
kumudgarg <kumud50garg@gmail.com>
2019-11-25 10:29:41 +0530

    in condition for computer and player winning some changes done

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -15,6 +15,8 @@ computerWinning=false
 counter=0
 valid=false
 isCornerEmpty=false
+checkCondition=false
+turn=0
 declare -A board
 function resetBoard()
 {
@@ -32,15 +34,14 @@ function whoPlayFirst()
 	if [ $toss -eq 0 ]
 	then		
 		echo "computerSymbol will play first"
-		counter=3
 		computerSymbol="0"
-		checkCornerOrCenterOrAnywhere
 		playerSymbol="x"
+		turn=0
 	else
 		echo "playerSymbol will play first"
-		counter=4
 		computerSymbol="x"
       		playerSymbol="0"
+		turn=1
 
 	fi
 }
@@ -60,46 +61,66 @@ function displayBoard()
 }
 function playTicTacToe()
 {
-	whoPlayFirst
+	whoPlayFirst 
  	local row=0
  	local column=0
- 	for (( turn=0; turn<$counter; turn++ ))
+ 	for (( count=0; count<$ARRAYLEN; count++ ))
  	do
-		displayBoard
-		read -p " enter player choice " playerChoice
-		row=$(( $playerChoice / $ROW ))
-		if [ $(( $playerChoice % $ROW )) -eq 0 ]
+		if [ $turn == 0 ]
 		then
-			row=$(( $row - 1 ))
-		fi 
-		column=$(( $playerChoice %  $COLUMN ))
-		if [ $column -eq 0 ]
-		then
-		column=$(( $column + 2 ))
-		else
-			column=$(( $column - 1 ))
-		fi
-		if [ $((${board[$row,$column]})) -eq $(($playerSymbol)) ]
-		then
-			echo "Invalid move"
-			(( turn-- ))
-		fi
-		board[$row,$column]=$playerSymbol
-		checkCornerOrCenterOrAnywhere
-      		if[ $isCornerEmpty == true ]
-      		then
-             		isCornerEmpty=false
-             		continue
-      		fi 
-   		checkWinAndLooseComputerMove $computerSymbol
-		checkWinAndLoose
-		conclusion
-		if [ $valid == true ]
-	   	then
-	         	return 0
-	    	fi
-		checkWinAndLooseComputerMove $playerSymbol
+			ischeckWinAndLooseResult $computerSymbol
+			if [ $valid == true ]
+			then
+				displayBoard
+				echo "you are loose"
+				exit
+			fi
+			checkWinAndLooseComputerMove $computerSymbol
+			if [ $isCellBlocked == true ]
+	                then
+				isCellBlocked=false
+			else
+				 checkCornerOrCenterOrAnywhere
+			fi
+			turn=1
+			ischeckWinAndLooseResult $computerSymbol
+                        if [ $valid == true ]
+                        then
+				displayBoard
+				echo "you are loose"
+                                exit
+                        fi
 
+		else
+			displayBoard
+			read -p " enter player choice " playerChoice
+			row=$(( $playerChoice / $ROW ))
+			if [ $(( $playerChoice % $ROW )) -eq 0 ]
+			then
+				row=$(( $row - 1 ))
+			fi 
+			column=$(( $playerChoice %  $COLUMN ))
+			if [ $column -eq 0 ]
+			then
+				column=$(( $column + 2 ))
+			else
+				column=$(( $column - 1 ))
+			fi
+			if [ $((${board[$row,$column]})) -eq $(($playerSymbol)) ] && [ ${board[$row,$column]} == $computerSymbol ]
+			then
+				echo "Invalid move"
+				(( count-- ))
+				continue
+			fi
+			board[$row,$column]=$playerSymbol
+			turn=0
+			ischeckWinAndLooseResult $playerSymbol
+ 			if [ $valid == true ]
+ 			then
+         			echo "you are loose"
+         			exit
+ 			fi
+		fi
 	done
 	displayBoard
 	echo "game Tie"
@@ -109,178 +130,106 @@ function ischeckWinAndLooseResult()
 	symbol=$1
 	if [ ${board[0,0]} == $symbol ]  && [ ${board[0,1]} == $symbol ] && [ ${board[0,2]} == $symbol ]
 	then
-		if [ $symbol == $playerSymbol ]
-		then
-			playerWinning=true
-		else
-			computerWinning=true
-		fi
+		valid=true
 	 elif [ ${board[1,0]} == $symbol ] && [ ${board[1,1]} == $symbol ] && [ ${board[1,2]} == $symbol ]
     	 then
-		if [ $symbol == $playerSymbol ]
-		then
-   		playerWinning=true
-		else
-   		computerWinning=true
-		fi
+		valid=true
 	 elif [ ${board[2,0]} == $symbol ] && [ ${board[2,1]} == $symbol ] && [ ${board[2,2]} == $symbol ]
     	 then
-	 	if [ $symbol == $playerSymbol ]
-      		then
-         		playerWinning=true
-      		else
-         		computerWinning=true
-		 fi
- 	elif [ ${board[0,0]} == $symbol ] && [ ${board[1,0]} == $symbol ] && [ ${board[2,0]} == $symbol ]
-	then
-		if [ $symbol == $playerSymbol ]
-		then
-			playerWinning=true
-		else
-			computerWinning=true
-		fi       
+		valid=true
+	 elif [ ${board[0,0]} == $symbol ] && [ ${board[1,0]} == $symbol ] && [ ${board[2,0]} == $symbol ]
+	 then
+		valid=true       
  	elif [ ${board[0,1]} == $symbol ] && [ ${board[1,1]} == $symbol ] && [ ${board[2,1]} == $symbol ]
     	then
-      		if [ $symbol == $playerSymbol ]
-      		then
-         		playerWinning=true
-      		else
-         		computerWinning=true
-      		fi	
+      		valid=true
  	elif [ ${board[0,2]} == $symbol ] && [ ${board[1,2]} == $symbol ] && [ ${board[2,2]} == $symbol ]
 	then
- 		if [ $symbol == $playerSymbol ]
-		then
- 			playerWinning=true
- 		else
-			computerWinning=true
- 		fi
+ 		valid=true
     	elif [ ${board[0,0]} == $symbol ] && [ ${board[1,1]} == $symbol ] && [ ${board[2,2]} == $symbol ]
     	then
-		if [ $symbol == $playerSymbol ]
-		then
-	      		playerWinning=true
-	  	else
-	      		computerWinning=true
-		fi
+		valid=true
 	elif [ ${board[0,2]} == $symbol ] && [ ${board[1,1]} == $symbol ] && [ ${board[2,0]} == $symbol ]
     	then
-    		if [ $symbol == $playerSymbol ]
-	   	then
-	   		playerWinning=true
-	   	else
-	   		computerWinning=true
-		fi       
+    		
+		valid=true
+	else
+		valid=false
 	fi
 }
 function checkWinAndLooseComputerMove()
 {
 	local row=0
 	local col=0
-	local sign=$1
 	#<----------------row wise checkWinAndLoose------------------------------------------------------------->  
 	for (( row=0; row<$ROW; row++ ))
 	do
-		if [ ${board[$row,$col]} == $sign ] && [ ${board[$row,$(( $col + 1 ))]} == $sign ]
-		then
+	if [ ${board[$row,$col]} == $playerSymbol ] && [ ${board[$row,$(( $col + 1 ))]} == $playerSymbol ]
+	then
 		board[$row,$(( $col + 2 ))]=$computerSymbol
-	elif [ ${board[$row,$(( $col + 1 ))]} == $sign ] && [ ${board[$row,$(( $col + 2 ))]} == $sign ]
+		isCellBlocked=true
+	elif [ ${board[$row,$(( $col + 1 ))]} == $playerSymbol ] && [ ${board[$row,$(( $col + 2 ))]} == $playerSymbol ]
 	then
 		board[$row,$col]=$computerSymbol
-	elif [ ${board[$row,$col]} == $sign ] && [ ${board[$row,$(( $col + 2 ))]} == $sign ]
+		isCellBlocked=true
+	elif [ ${board[$row,$col]} == $playerSymbol ] && [ ${board[$row,$(( $col + 2 ))]} == $playerSymbol ]
 	then
 		board[$row,$(( $col + 1 ))]=$computerSymbol
+		isCellBlocked=true
 	fi
 	done
 	#<--------------------column wise checkWinAndLoose--------------------------------------------------------->
 	row=0
-   	for (( col=0; col<$COLUMN; col++ ))
+	for (( col=0; col<$ROW; col++ ))
    	do
-      		if [ ${board[$row,$col]} == $sign ] && [ ${board[$(( $row+1 )),$col]} == $sign ]
+      		if [ ${board[$row,$col]} == $playerSymbol ] && [ ${board[$(( $row+1 )),$col]} == $playerSymbol ]
       		then
-         		board[$(( $row+2 )),$col]=$computerSymbol
-      		elif [ ${board[$row,$col]} == $sign ] && [ ${board[$(($row+2)),$col]} == $sign ]
+			board[$(( $row+2 )),$col]=$computerSymbol
+			isCellBlocked=true
+      		elif [ ${board[$row,$col]} == $playerSymbol ] && [ ${board[$(($row+2)),$col]} == $playerSymbol ]
       		then
-         		board[$(($row+1)),$col]=$computerSymbol
-		elif [ ${board[$(($row+1)),$col]} == $sign ] && [ ${board[$(($row+2)),$col]} == $sign ]
+			board[$(($row+1)),$col]=$computerSymbol
+			isCellBlocked=true
+		elif [ ${board[$(($row+1)),$col]} == $playerSymbol ] && [ ${board[$(($row+2)),$col]} == $playerSymbol ]
 	      	then
-	        	board[$row,$col]=$computerSymbol
-      		fi
+			board[$row,$col]=$computerSymbol
+			isCellBlocked=true
+     		fi
    	done
 	#<-----------------------------Diagonally checkWinAndLoose------------------------------------------------------>
 	col=0
-	if [ ${board[$row,$col]} == $sign ] && [ ${board[$(( $row+1 )),$(( $col+1 ))]} == $sign ]
+	if [ ${board[$row,$col]} == $playerSymbol ] && [ ${board[$(( $row+1 )),$(( $col+1 ))]} == $playerSymbol ]
 	then
 		board[$(( $row+2 )),$(( $col+2 ))]=$computerSymbol
-	elif [ ${board[$row,$col]} == $sign ] && [ ${board[$(($row+2)),$(($col+2))]} == $sign ]
+		isCellBlocked=true
+	elif [ ${board[$row,$col]} == $playerSymbol ] && [ ${board[$(($row+2)),$(($col+2))]} == $playerSymbol ]
 	then
 		board[$(($row+1)),$(($col+1))]=$computerSymbol
-	elif [ ${board[$(($row+1)),$(($col+1))]} == $sign ] && [ ${board[$(($row+2)),$(($col+2))]} == $sign ]
+		isCellBlocked=true
+	elif [ ${board[$(($row+1)),$(($col+1))]} == $playerSymbol ] && [ ${board[$(($row+2)),$(($col+2))]} ==  $playerSymbol ]
 	then
 		board[$row,$col]=$computerSymbol
-	elif [ ${board[$row,$(($col+2))]} == $sign ] && [ ${board[$(($row+1)),$(($col+1))]} == $sign ]
+		isCellBlocked=true
+	elif [ ${board[$row,$(($col+2))]} == $playerSymbol ] && [ ${board[$(($row+1)),$(($col+1))]} == $playerSymbol ]
 	then
 		board[$(($row+2)),$col]=$computerSymbol
-	elif [ ${board[$row,$(($col+2))]} == $sign ] && [ ${board[$(($row+2)),$col]} == $sign ]
+		isCellBlocked=true
+	elif [ ${board[$row,$(($col+2))]} == $playerSymbol ] && [ ${board[$(($row+2)),$col]} == $playerSymbol ]
 	then
 		board[$(($row+1)),$(($col+1))]=$computerSymbol
-	elif [ ${board[$(($row+2)),$col]} == $sign ] && [ ${board[$(($row+1)),$(($col+1))]} == $sign ]
+		isCellBlocked=true
+	elif [ ${board[$(($row+2)),$col]} == $playerSymbol ] && [ ${board[$(($row+1)),$(($col+1))]} == $playerSymbol ]
 	then
 		board[$row,$(($col+2))]=$computerSymbol
-	fi
-}
-function isFilledCell()
-{
-	num=0
-	for (( i=0; i<$ROW; i++ ))
-	do
-		for (( j=0; j<$COLUMN; j++ ))
-		do
-			 if [ ${board[$i,$j]} == $playerSymbol ] || [ ${board[$i,$j]} == $computerSymbol ]
-          		then
-				num=$(( $num+1 ))
-			 fi
-		done
-	done
-	if [ $num == 9 ]
-	then
-		echo true
+		isCellBlocked=true
 	else
-		echo false
+		isCellBlocked=false
+
 	fi
-}
-function conclusion()
-{
-	if [ $playerWinning == true ]
-	then
-   		displayBoard
-   		echo "you are won"
-   		valid=true
-	elif [ $computerWinning == true ]
- 	then
-		displayBoard
-		echo "you are loose"
-		valid=true
-	fi 
-}
-function checkWinAndLoose()
-{
-	i=1
-	while [ $i -le 2 ]
-	do
-		if [ $i == 1 ]
-		then
-			$(ischeckWinAndLooseResult $playerSymbol $computerSymbol)
-		elif [ $i == 2 ]
-		then
-  			$(ischeckWinAndLooseResult $computerSymbol $playerSymbol)
-		fi
-		(( i++ ))
-	done
 }
 function checkCornerOrCenterOrAnywhere()
 {
-	if [ ${board[0,0]} != $playerSymbol ] && [ ${board[0,0]} != $computerSymbol ]
+	if [[ ${board[0,0]} != $playerSymbol ]] && [[ ${board[0,0]} != $computerSymbol ]]
 	then
 		board[0,0]=$computerSymbol
 		isCornerEmpty=true
@@ -316,6 +265,7 @@ function checkCornerOrCenterOrAnywhere()
 	then
 		board[3,1]=$computerSymbol
 		isCornerEmpty=true
+	fi
 }
 resetBoard
 playTicTacToe
